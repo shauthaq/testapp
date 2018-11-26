2fba4dcc27928dfec61e28329f0c1204a20c62f0
shoichi <shauthaq@gmail.com>
2018-11-26 21:43:33 +0900

    viewのyieldやsectionつかった遊び

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,21 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Hello/Index</title>
-	<style>
-	body{font-size: 16px; color:#999;}
-	h1{font-size: 100pt; text-align: right; color:#f6f6f6;
-		margin:-50px 0px -100px 0px;}
-	</style>
-</head>
-<body>
-	<h1>Index</h1>
-	<p>{{$msg}}</p>
-	<form action="/hello" method="post" accept-charset="utf-8">
-	{{csrf_field()}}		
-	<input type="text" name="name" value="@isset($name){{$name}}@endisset">
-	{{ Form::text('username') }}
-	<input type="submit">
-	</form>
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+	@parent
+	インデックスページ
+	さらにインデックスページ
+@endsection
+
+@section('content')
+	<p>ここが本文のコンテンツです</p>
+	<p>必要なだけ記述ができます</p>
+@endsection
+
+@section('footer')
+	copyright 2018 shoichi.ltd.
+@endsection
